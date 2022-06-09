549d81bd06d82c250764e77eb515ecd1f0cd09be
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-06-09 20:48:10 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,19 +1,36 @@
 // node a.js
 
+// const prompt_sync = require('prompt-sync')();
+// let input = prompt_sync('ต้องการสูตรคูณแม่อะไร >>> ');
+// let length = prompt_sync('จำนวน >>> ')
+
+// let title = input
+// let use = input
+
+// console.log(`สูตรคูณแม่ ${title}`);
+
+// function process(result) {
+//     for (index = 0; index < length;) {
+//         index++
+//         result = index * use
+//         console.log(`${use} X ${index} =  ${result}`);
+//     };
+// };
+// process();
+
 const prompt_sync = require('prompt-sync')();
-let input = prompt_sync('ต้องการสูตรคูณแม่อะไร >>> ');
-let length = prompt_sync('จำนวน >>> ')
+class multiplication {
+    input = prompt_sync('เลือกสูตรคูณแม่ >>> ');
+    length = prompt_sync('จำนวน >>> ')
 
-let title = input
-let use = input
-
-console.log(`สูตรคูณแม่ ${title}`);
-
-function process(result) {
-    for (index = 0; index < length;) {
-        index++
-        result = index * use
-        console.log(`${use} X ${index} =  ${result}`);
+    process = (result, zero = 0) => {
+        console.log(`\nสูตรคูณแม่ ${this.input}`);
+        while (zero < this.length) {
+            zero++
+            result = zero * parseInt(this.input);
+            console.log(`${this.input} X ${zero} =  ${result}`);
+        };
     };
 };
-process();
+
+new multiplication().process();
