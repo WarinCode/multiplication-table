d6dde15eac9d91a63650164ced54906d0ae64f96
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-06-22 14:14:35 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# multiplication-table
+can find multiplication table
+
+<img src="https://raw.githubusercontent.com/Varin471/multiplication-table/main/img.jpg">
---
+++ python/script.py
@@ -0,0 +1,12 @@
+# RUNFILE: python script.py
+
+number  = int(input("สูตรคูณแม่  = "))
+lenght = int(input("จำนวน = "))
+render = number
+i = 1
+print("\nสูตรคูณแม่ ", render)
+while i <= lenght:
+    result = number * i
+    print(number , "X", i , "=" ,result)
+    i +=1
+    
