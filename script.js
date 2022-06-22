8a31f298867ccefd39666fd96abc17337bde1a23
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-06-22 14:04:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// node a.js
-
-// const prompt_sync = require('prompt-sync')();
-// let input = prompt_sync('ต้องการสูตรคูณแม่อะไร >>> ');
-// let length = prompt_sync('จำนวน >>> ')
-
-// let title = input
-// let use = input
-
-// console.log(`สูตรคูณแม่ ${title}`);
-
-// function process(result) {
-//     for (index = 0; index < length;) {
-//         index++
-//         result = index * use
-//         console.log(`${use} X ${index} =  ${result}`);
-//     };
-// };
-// process();
+// RUNFILE: node script.js
 
 const prompt_sync = require('prompt-sync')();
 class multiplication {
